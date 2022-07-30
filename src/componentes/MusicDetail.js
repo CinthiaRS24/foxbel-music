37a02cb0438300a3698db7b59e3c3dbb78304356
Cinthia Ramos <cinthia24027@gmail.com>
2022-07-30 10:10:39 -0500

    styles in MusicDetail

---
+++ src/componentes/MusicDetail.js
@@ -0,0 +1,12 @@
+import s from './MusicDetail.module.css'
+
+export default function MusicDetail () {
+
+    return (
+        <div className={s.divGeneral}>
+            <img className={s.img} src='https://api.time.com/wp-content/uploads/2021/10/GettyImages-1229269763.jpg'/>
+            <div className={s.infoDetail}>
+            </div>
+        </div>
+    )
+}
