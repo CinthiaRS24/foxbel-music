62828f92a876904722f1cde20503728e7846744a
Cinthia Ramos <cinthia24027@gmail.com>
2022-07-31 00:50:39 -0500

    add logic and improve styles in Card

--- src/componentes/Card.js
+++ src/componentes/Card.js
@@ -1,12 +1,31 @@
+import { useDispatch, useSelector } from "react-redux";
+import imgDefault from '../images/imgDefault.jpg'
+import {playTrack} from '../redux/actions'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlay, faEllipsisV } from '@fortawesome/free-solid-svg-icons'
+
 import s from './Card.module.css'
 
-export default function Card () {
+export default function Card ({img, title, artist, url}) {
+
+    const dispatch = useDispatch();
+
+    const handlerPlay = (url) =>  {
+        dispatch(playTrack(url))
+    }
+
     return (
         <div className="card">
-            <img className="card-img-top" src="https://api.time.com/wp-content/uploads/2021/10/GettyImages-1229269763.jpg" alt="Card image cap"/>
+            <div className={s.figure}>
+            <img className="card-img-top" src={img? img : imgDefault} alt="Card image cap" onClick={() => handlerPlay({img, title, artist, url})}/>
+            <FontAwesomeIcon icon={faPlay} color='white' className={s.iconPlay} />
+            <div  className={s.iconPoints}>
+            <FontAwesomeIcon icon={faEllipsisV} color='white'  />
+            </div>
+            </div>
             <div className="card-body">
-                <h3 className="card-text">21</h3>
-                <h4>Adele</h4>
+                <h3 className="card-text">{title}</h3>
+                <h4>{artist}</h4>
             </div>
         </div>
     )
