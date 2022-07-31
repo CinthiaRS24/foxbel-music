b6c6c6daa987612d1f0bfda4b8849df62cfac7d6
Cinthia Ramos <cinthia24027@gmail.com>
2022-07-31 00:54:55 -0500

    add logic and improve styles in MusicDetail

--- src/componentes/MusicDetail.js
+++ src/componentes/MusicDetail.js
@@ -1,11 +1,23 @@
+import { useSelector } from 'react-redux';
+import imgDefault from '../images/imgDefault.jpg'
 import s from './MusicDetail.module.css'
 
 export default function MusicDetail () {
 
+    const trackSelected = useSelector(state => state.track);
+    const artistDefault = "Selecciona una música"
+
     return (
         <div className={s.divGeneral}>
-            <img className={s.img} src='https://api.time.com/wp-content/uploads/2021/10/GettyImages-1229269763.jpg'/>
+            <img className={s.img} src={trackSelected.img? trackSelected.img : imgDefault}/>
             <div className={s.infoDetail}>
+                <h3 className={s.artistName}>{trackSelected.artist? trackSelected.artist : artistDefault}</h3>
+                <h3 className={s.info1}>{trackSelected.artist? `Lo mejor de ${trackSelected.artist}` : null}</h3>
+                <h3 className={s.info2}>{trackSelected.artist? `${trackSelected.artist} (Tottenham, Londres, Inglaterra, 5 de mayo de 1988), conocida simplemente como Adele, es una cantante, compositora y multinstrumentista británica.` : null}</h3>
+                <div className={s.divButtons}>
+                    <button className={s.btn1}>Reproducir</button>
+                    <button className={s.btn2}>Seguir</button>
+                </div>
             </div>
         </div>
     )
