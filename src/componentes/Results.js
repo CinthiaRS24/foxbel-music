c340148ac9cb1c99e354167dc9e7af869d7548c0
Cinthia Ramos <cinthia24027@gmail.com>
2022-07-31 00:56:09 -0500

    add logic in Results

--- src/componentes/Results.js
+++ src/componentes/Results.js
@@ -1,16 +1,22 @@
+import { useSelector } from 'react-redux';
+import imgDefault from '../images/imgDefault.jpg'
 import Card from './Card'
 import s from './Results.module.css'
 
 export default function Results () {
-    const cartas = [1, 2, 3, 4, 5, 6, 7];
+    const resultados = useSelector(state => state.results.data);
+    let img = {imgDefault}
 
     return (
         <div className={s.divGeneral}>
             <h1 className={s.title}>Resultados</h1>
 
-            <div className={s.cards}>
-                {cartas.map(card =>
-                {return <Card/>}
+            <div className={s.cards} >
+                {resultados?.map(card => {
+                    {card.album? img = card.album.cover: img = imgDefault}
+                    return (
+                        <Card img={img} title={card.title} artist={card.artist.name} url={card.preview} key={card.id}/>
+                )}
                 )}
             </div>
             
