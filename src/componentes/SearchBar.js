27a88626dc3be501f16dc0dc8aebb61b70893324
Cinthia Ramos <cinthia24027@gmail.com>
2022-07-31 00:56:37 -0500

    add logic in SearchBar

--- src/componentes/SearchBar.js
+++ src/componentes/SearchBar.js
@@ -1,13 +1,33 @@
 import user from '../images/user.png'
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import {getSongs} from '../redux/actions'
 import s from './SearchBar.module.css'
 
 export default function SearchBar () {
+
+    const dispatch = useDispatch();
+    const [searched, setSerched] = useState("");
+
+    function handlerChange(e) {
+        e.preventDefault();
+        setSerched(e.target.value);
+        console.log(searched);
+    }
+
+    function handlerSubmit(e) {
+        e.preventDefault();
+        dispatch(getSongs(`search?q=${searched}`));
+    }
+
     return(
         <div className={s.divGeneral}>
             <div className="input-group rounded">
-                <input type="search" className="form-control rounded" placeholder="Buscar" aria-label="Search" aria-describedby="search-addon" />
-                <span className="input-group-text border-0" id="search-addon">
-                </span>
+                <form onSubmit={(e) => handlerSubmit(e)}>
+                    <input type="search" className="form-control rounded" placeholder="Buscar" onChange={(e) => handlerChange(e)} aria-label="Search" aria-describedby="search-addon" />
+                    <span className="input-group-text border-0" id="search-addon">
+                    </span>
+                </form>
             </div>
 
             <div className={s.user}>
