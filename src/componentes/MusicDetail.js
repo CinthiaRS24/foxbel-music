2756c942b2745e5c3e23dbd3fa97c1e8c4862153
Cinthia Ramos <cinthia24027@gmail.com>
2022-08-01 11:20:00 -0500

    add play and pause icon in MusicDetail

--- src/componentes/MusicDetail.js
+++ src/componentes/MusicDetail.js
@@ -1,15 +1,32 @@
 import { useSelector } from 'react-redux';
 import imgDefault from '../images/imgDefault.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPause, faPlay } from '@fortawesome/free-solid-svg-icons'
 import s from './MusicDetail.module.css'
 
 export default function MusicDetail () {
 
     const trackSelected = useSelector(state => state.track);
+    const play = useSelector(state => state.play);
     const artistDefault = "Selecciona una música"
 
     return (
         <div className={s.divGeneral}>
             <img className={s.img} src={trackSelected.img? trackSelected.img : imgDefault}/>
+            {
+               trackSelected.artist?
+                    <div className={s.iconPlay}>
+                        {
+                            play?
+                                <FontAwesomeIcon icon={faPause} color='white' className={s.buttons} />
+                            :
+                                <FontAwesomeIcon icon={faPlay} color='white' className={s.iconPlay} />
+                        }  
+                    </div> 
+                :
+                    null
+            }
+    
             <div className={s.infoDetail}>
                 <h3 className={s.artistName}>{trackSelected.artist? trackSelected.artist : artistDefault}</h3>
                 <h3 className={s.info1}>{trackSelected.artist? `Lo mejor de ${trackSelected.artist}` : null}</h3>
