735d820144c189a26cc34a7a92c1fcd57eb7a2f0
Cinthia Ramos <cinthia24027@gmail.com>
2022-08-03 14:49:13 -0500

    improve styles in NavigationMenu

--- src/componentes/NavigationMenu.js
+++ src/componentes/NavigationMenu.js
@@ -2,12 +2,14 @@ import s from './NavigationMenu.module.css'
 import logo from '../images/foxbel-music@3x.png'
 import {getSongs} from '../redux/actions'
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function NavigationMenu () {
 
     const dispatch = useDispatch();
     const results = useSelector(state => state.results);
+
+    const [selected, setSelected] = useState(1)
 
     function handlerSongs(e, params) {
         if (e) {
@@ -26,19 +28,19 @@ export default function NavigationMenu () {
             <div className={s.divLibreria}>
                 <p>Mi Librería</p>
                 <ul className="nav flex-column">
-                    <li className="nav-item" onClick={(e) => handlerSongs(e, 'radio/37151/tracks')}>
+                    <li className={selected === 1? s.selected : null} onClick={(e) => {handlerSongs(e, 'radio/37151/tracks'); setSelected(1)}}>
                         <a className="nav-link active" href="#">Recientes</a>
                     </li>
-                    <li className="nav-item list-unstyled" onClick={(e) => handlerSongs(e, 'artist/27/top?limit=25')}>
+                    <li className={selected === 2? s.selected : null} onClick={(e) => {handlerSongs(e, 'artist/27/top?limit=25'); setSelected(2)}}>
                         <a className="nav-link" href="#">Artistas</a>
                     </li>
-                    <li className="nav-item" onClick={(e) => handlerSongs(e, 'album/302127/tracks')}>
+                    <li className={selected === 3? s.selected : null} onClick={(e) => {handlerSongs(e, 'album/302127/tracks'); setSelected(3)}}>
                         <a className="nav-link" href="#">Albums</a>
                     </li>
-                    <li className="nav-item" onClick={(e) => handlerSongs(e, 'chart/0/tracks')}>
+                    <li className={selected === 4? s.selected : null} onClick={(e) => {handlerSongs(e, 'chart/0/tracks'); setSelected(4)}}>
                         <a className="nav-link" href="#">Canciones</a>
                     </li>
-                    <li className="nav-item" onClick={(e) => handlerSongs(e, 'radio/31061/tracks')}>
+                    <li className={selected === 5? s.selected : null} onClick={(e) => {handlerSongs(e, 'radio/31061/tracks'); setSelected(5)}}>
                         <a className="nav-link" href="#">Estaciones</a>
                     </li>
                 </ul>
@@ -47,16 +49,16 @@ export default function NavigationMenu () {
             <div className={s.divPlaylist}>
                 <p>Playlist</p>
                 <ul className="nav flex-column">
-                    <li className="nav-item" onClick={(e) => handlerSongs(e, 'search?q=metal')}>
+                    <li className={selected === 6? s.selected : null} onClick={(e) => {handlerSongs(e, 'search?q=metal'); setSelected(6)}}>
                         <a className="nav-link" href="#">Metal</a>
                     </li>
-                    <li className="nav-item" onClick={(e) => handlerSongs(e, 'search?q=dance')}>
+                    <li className={selected === 7? s.selected : null} onClick={(e) => {handlerSongs(e, 'search?q=dance'); setSelected(7)}}>
                         <a className="nav-link" href="#">Para bailar</a>
                     </li>
-                    <li className="nav-item" onClick={(e) => handlerSongs(e, 'search?q=rock')}>
+                    <li className={selected === 8? s.selected : null} onClick={(e) => {handlerSongs(e, 'search?q=rock'); setSelected(8)}}>
                         <a className="nav-link" href="#">Rock 90s</a>
                     </li>
-                    <li className="nav-item" onClick={(e) => handlerSongs(e, 'search?q=baladas')}>
+                    <li className={selected === 9? s.selected : null} onClick={(e) => {handlerSongs(e, 'search?q=baladas'); setSelected(9)}}>
                         <a className="nav-link" href="#">Baladas</a>
                     </li>
                 </ul>
